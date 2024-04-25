6dc439561e6bcd198c7c25f99efeca4f1524d7e7
BrettCross <brettcross@cmail.carleton.ca>
2024-04-24 22:45:27 -0400

    Update UI layout & style

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, SafeAreaView, Pressable } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.text}>Email or username</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="example@gmail.com"
+          placeholderTextColor='#9b9b9b'/>
+
+        <Text style={styles.text}>Password</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="minimum 6 characters"
+          placeholderTextColor='#9b9b9b'/>
+
+        <Button 
+          title="Forgot Password?"
+          color="#9b9b9b"
+          onPress={() => null}
+        />
+
+        <Pressable style={styles.button}>
+          <Text style={styles.text}>Login</Text>
+        </Pressable>
+        
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    alignContent: 'center',
+    justifyContent: 'flex-start',
+    padding: 16
+  },
+  text: {
+    color: '#fff',
+    fontSize: 18,
+    lineHeight: 21,
+    margin: 8,
+  },
+  textInput: {
+    fontSize: 18,
+    margin: 8,
+    paddingVertical: 8,
+    color: 'red',
+    borderBottomColor: '#9b9b9b',
+    borderBottomWidth: 1
+  },
+  button: { 
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 32,
+    margin: 8,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#9b9b9b' 
   },
 });
